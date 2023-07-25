13845280290ecb2db7a722ab8872022db7d268d6
luscask332 <139006313+luscask332@users.noreply.github.com>
2023-07-25 17:31:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openDialog() {
+    const dialog = document.getElementById("dialog");
+    dialog.style.display = "flex";
+}
+
+function closeDialog() {
+    const dialog = document.getElementById("dialog");
+    dialog.style.display = "none";
+}
