9f5544c97b70ebed607b46dcab6003070250c16d
luscask332 <139006313+luscask332@users.noreply.github.com>
2023-07-25 18:38:58 -0300

    Delete zavas.js

--- zavas.js
+++
@@ -1,9 +0,0 @@
-function openDialog() {
-    const dialog = document.getElementById("dialog");
-    dialog.style.display = "flex";
-}
-
-function closeDialog() {
-    const dialog = document.getElementById("dialog");
-    dialog.style.display = "none";
-}
